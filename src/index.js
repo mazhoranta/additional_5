eaa957078ec85d4dbc8c8f364e7d5c7314c5219b
mazhoranta <mazhoranta@gmail.com>
2018-09-19 02:44:16 +0300

    write function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brackets = str.split("");
+  var bracketsStack = [];
+  
+  outer:
+  for (var i = 0; i < brackets.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      var configElement = bracketsConfig[j];
+      if (brackets[i] == configElement[0] && !~bracketsStack.indexOf(configElement[1])) {
+        bracketsStack.push(brackets[i]);
+        continue outer;
+      } 
+      if (brackets[i] == configElement[1]) {
+        if (bracketsStack.pop() == configElement[0])
+          continue outer;
+        else
+          return false;
+      }
+    }
+  }
+
+  if (bracketsStack.length > 0)
+    return false;
+
+  return true;
 }
